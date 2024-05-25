2af93b4b3abeb48754d0264574f70434c8fc0f53
Anuj-jnv <anujk534208@gmail.com>
2024-05-25 20:57:21 +0530

    2- Auth-Controllers Added

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,9 +1,12 @@
 import express from "express"
-import { signup ,login, logout} from "../controllers/auth.controllers.js";
+import { signup ,login, logout,getMe} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 
 const router= express.Router();
 
-router.get("/signup",signup)
+router.get("/me",protectRoute,getMe)
+
+router.post("/signup",signup)
 
 router.post("/login",login)
 
--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,25 @@ import express from "express"
 import authRoutes from "./routes/auth.routes.js";
 import dotenv from "dotenv";
 import connectDB from "./db/connectMongoDb.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
-const app =express();
 connectDB();
+
+const app =express();
+
+// middlewares
+app.use(express.json({ limit: "5mb" }));//to parse req body
+app.use(express.urlencoded({ extended: true }));//to parse form data(urlencoded)
+app.use(cookieParser());
 
 const port=process.env.PORT || 8080;
 
-app.get('/',(rq,res)=>{
-    res.send("Hello World");
-})
+
+
 app.use("/api/auth",authRoutes);
+
+
 
 app.listen(port,()=>{
     console.log(`server is listening on ${port}`);
