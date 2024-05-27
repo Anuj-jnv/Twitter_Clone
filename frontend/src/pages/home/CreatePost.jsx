82b86890793750f34575d15e15226fd230c760fb
Anuj-jnv <anujk534208@gmail.com>
2024-05-27 12:10:26 +0530

    6- fetch posts for home pages

--- frontend/src/pages/home/CreatePost.jsx
+++ frontend/src/pages/home/CreatePost.jsx
@@ -1,80 +1,73 @@
-import { CiImageOn } from "react-icons/ci";
-import { BsEmojiSmileFill } from "react-icons/bs";
-import { useRef, useState } from "react";
-import { IoCloseSharp } from "react-icons/io5";
+import Post from "./Post";
+import PostSkeleton from "../skeletons/PostSkeleton";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
 
-const CreatePost = () => {
-	const [text, setText] = useState("");
-	const [img, setImg] = useState(null);
-
-	const imgRef = useRef(null);
-
-	const isPending = false;
-	const isError = false;
-
-	const data = {
-		profileImg: "/avatars/boy1.png",
-	};
-
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		alert("Post created successfully");
-	};
-
-	const handleImgChange = (e) => {
-		const file = e.target.files[0];
-		if (file) {
-			const reader = new FileReader();
-			reader.onload = () => {
-				setImg(reader.result);
-			};
-			reader.readAsDataURL(file);
+const Posts = ({ feedType, username, userId }) => {
+	const getPostEndpoint = () => {
+		switch (feedType) {
+			case "forYou":
+				return "/api/posts/all";
+			case "following":
+				return "/api/posts/following";
+			case "posts":
+				return `/api/posts/user/${username}`;
+			case "likes":
+				return `/api/posts/likes/${userId}`;
+			default:
+				return "/api/posts/all";
 		}
 	};
 
-	return (
-		<div className='flex p-4 items-start gap-4 border-b border-gray-700'>
-			<div className='avatar'>
-				<div className='w-8 rounded-full'>
-					<img src={data.profileImg || "/avatar-placeholder.png"} />
-				</div>
-			</div>
-			<form className='flex flex-col gap-2 w-full' onSubmit={handleSubmit}>
-				<textarea
-					className='textarea w-full p-0 text-lg resize-none border-none focus:outline-none  border-gray-800'
-					placeholder='What is happening?!'
-					value={text}
-					onChange={(e) => setText(e.target.value)}
-				/>
-				{img && (
-					<div className='relative w-72 mx-auto'>
-						<IoCloseSharp
-							className='absolute top-0 right-0 text-white bg-gray-800 rounded-full w-5 h-5 cursor-pointer'
-							onClick={() => {
-								setImg(null);
-								imgRef.current.value = null;
-							}}
-						/>
-						<img src={img} className='w-full mx-auto h-72 object-contain rounded' />
-					</div>
-				)}
+	const POST_ENDPOINT = getPostEndpoint();
 
-				<div className='flex justify-between border-t py-2 border-t-gray-700'>
-					<div className='flex gap-1 items-center'>
-						<CiImageOn
-							className='fill-primary w-6 h-6 cursor-pointer'
-							onClick={() => imgRef.current.click()}
-						/>
-						<BsEmojiSmileFill className='fill-primary w-5 h-5 cursor-pointer' />
-					</div>
-					<input type='file' hidden ref={imgRef} onChange={handleImgChange} />
-					<button className='btn btn-primary rounded-full btn-sm text-white px-4'>
-						{isPending ? "Posting..." : "Post"}
-					</button>
+	const {
+		data: posts,
+		isLoading,
+		refetch,
+		isRefetching,
+	} = useQuery({
+		queryKey: ["posts"],
+		queryFn: async () => {
+			try {
+				const res = await fetch(POST_ENDPOINT);
+				const data = await res.json();
+
+				if (!res.ok) {
+					throw new Error(data.error || "Something went wrong");
+				}
+
+				return data;
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+	});
+
+	useEffect(() => {
+		refetch();
+	}, [feedType, refetch, username]);
+
+	return (
+		<>
+			{(isLoading || isRefetching) && (
+				<div className='flex flex-col justify-center'>
+					<PostSkeleton />
+					<PostSkeleton />
+					<PostSkeleton />
 				</div>
-				{isError && <div className='text-red-500'>Something went wrong</div>}
-			</form>
-		</div>
+			)}
+			{!isLoading && !isRefetching && posts?.length === 0 && (
+				<p className='text-center my-4'>No posts in this tab. Switch 👻</p>
+			)}
+			{!isLoading && !isRefetching && posts && (
+				<div>
+					{posts.map((post) => (
+						<Post key={post._id} post={post} />
+					))}
+				</div>
+			)}
+		</>
 	);
 };
-export default CreatePost;
+export default Posts;
