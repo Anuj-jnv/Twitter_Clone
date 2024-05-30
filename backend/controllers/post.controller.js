dd05f6a76b1c03707ccc66bfe01ad8172d065291
Anuj-jnv <anujk534208@gmail.com>
2024-05-30 22:36:25 +0530

    changed

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -210,7 +210,7 @@ export const getUserPosts = async (req, res) => {
 		const user = await User.findOne({ username });
 		if (!user) return res.status(404).json({ error: "User not found" });
 
-		const posts = await Post.find({ user: user._id })
+		const posts = await Post.find({ user: user?._id })
 			.sort({ createdAt: -1 })
 			.populate({
 				path: "user",
--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -137,7 +137,7 @@ export const updateUser = async (req, res) => {
 		user.username = username || user.username;
 		user.bio = bio || user.bio;
 		user.link = link || user.link;
-		user?.profileImg = profileImg || user?.profileImg;
+		user.profileImg = profileImg || user?.profileImg;
 		user.coverImg = coverImg || user.coverImg;
 
 		user = await user.save();
